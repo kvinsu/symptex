e91af4d150c98cc08d0d6d3cc5932e046e173933
Kevin Su <ga27tig@mytum.de>
2024-11-26 11:40:39 +0100

    Add memory powered by LangGraph

--- api/app/routers/chat.py
+++ api/app/routers/chat.py
@@ -1,8 +1,13 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, HTTPException
 from fastapi.responses import StreamingResponse
+from langchain_core.messages import HumanMessage
 from pydantic import BaseModel
+import logging
 
-from api.chains.symptex_chain import get_chat_chain
+from api.chains.symptex_chain import app
+
+logger = logging.getLogger('uvicorn.error')
+logger.setLevel(logging.DEBUG)
 
 router = APIRouter()
 
@@ -11,15 +16,22 @@ router = APIRouter()
 class ChatRequest(BaseModel):
     message: str
 
+async def stream_response(message: str):
+    # TODO thread_id for each session
+    config = {"configurable": {"thread_id": "1"}}
+    logger.debug("STREAMING")
 
-# Dependency to get the chain
-def chat_chain():
-    return get_chat_chain()
-
+    async for msg, metadata in app.astream(
+        {"messages": [HumanMessage(message)]}, config, stream_mode="messages"):
+        # Get AIMessageChunks only
+        if msg.content and not isinstance(msg, HumanMessage):
+            logger.debug(msg.content)
+            yield msg.content
 
 @router.post("/chat")
-async def chat_with_llm(request: ChatRequest, chain=Depends(chat_chain)):
+async def chat_with_llm(request: ChatRequest):
     try:
-        return StreamingResponse(chain.astream(request.message), media_type="text/plain")
+        logger.debug("Received request")
+        return StreamingResponse(stream_response(request.message), media_type="text/plain")
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
--- api/chains/symptex_chain.py
+++ api/chains/symptex_chain.py
@@ -1,50 +1,59 @@
+from langchain_core.prompts import MessagesPlaceholder
 from langchain_ollama import ChatOllama
 from langchain.prompts import (
     SystemMessagePromptTemplate,
-    HumanMessagePromptTemplate,
     ChatPromptTemplate,
 )
-from langchain_core.output_parsers import StrOutputParser
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.graph import START, MessagesState, StateGraph
 
 OLLAMA_URL = "http://host.docker.internal:11434"
 
-CHAT_PROMPT_TEMPLATE = ChatPromptTemplate.from_messages(
+PROMPT = ChatPromptTemplate.from_messages(
     [
         SystemMessagePromptTemplate.from_template(
             """
-            You are a patient who is currently experiencing a health issue and is speaking with a doctor. 
-            Your goal is to answer the doctor's questions directly, without offering unnecessary details unless prompted.
-            Do not pose questions and do not answer questions not related to your health. 
-            Your answers should be short, specific, and focused on the question asked.
-            Avoid offering explanations or speculations like "I think it could be due to X, Y, Z", unless specifically asked about causes or history.
-            Keep your responses realistic and focused on the symptoms without elaborating too much on causes, diagnoses, or irrelevant details.
-            If you don't know an answer, say you don't know. 
-    
-            You have the following characteristics: 
-            - Age: 30
-            - Symptoms: Cough, Fever
-            - Duration of Symptoms: 2 weeks
-            - Relevant Medical History: Asthma
+            Du bist ein Patient, kein Arzt, der derzeit ein gesundheitliches Problem hat und mit einem Arzt spricht.
+            Dein Ziel ist es, dem Krankheitsverlauf und den Vorerkrankungen zufolge realistisch als Patient zu agieren, 
+            und kommende Fragen des Arztes ohne unnötige Details direkt zu beantworten, es sei denn, diese werden ausdrücklich erfragt.
+            Stelle keine Fragen und beantworte auf gar keinen Fall Fragen, die nichts mit deinem Gesundheitszustand zu 
+            tun haben, auch wenn der Arzt darauf besteht.
+            Deine Antworten sollten kurz, realistisch, präzise und auf die gestellte Frage fokussiert sein.
+            Vermeide es, Erklärungen oder Vermutungen wie „Ich denke, es könnte an X, Y, Z liegen“ anzubieten, es sei 
+            denn, du wirst ausdrücklich nach möglichen Ursachen oder deiner Vorgeschichte gefragt.
+            Wenn du eine Antwort nicht weißt, sag einfach, dass du es nicht weißt.
+            
+            Du hast folgende Eigenschaften:
+            
+            Alter: 82 Jahre
+            Krankheitsverlauf: Heute morgen auf die rechte Seite gestürzt, mit dem RTW in die Klinik geliefert
+            Relevante Voerkrankungen: Art. Hypertonie, sehr ausgeprägte senile Alzheimer Demenz, chronische Niereninsuffizienz G3A2, Varikosis bds.
             """
         ),
-        HumanMessagePromptTemplate.from_template("{input}"),
+        MessagesPlaceholder("messages"),
     ]
 )
 
+llm = ChatOllama(
+    base_url=OLLAMA_URL,
+    model="llama3.1",
+    temperature=0.5,
+)
 
-# Chain definition
-def get_chat_chain():
-    # Create LLM with parameters
-    llm = ChatOllama(
-        base_url=OLLAMA_URL,
-        model="llama3",
-        temperature=0.5,
-    )
+workflow = StateGraph(state_schema=MessagesState)
 
-    # Create parser to parse out String response from AIMessage object
-    parser = StrOutputParser()
 
-    # Create chains with prompt template, model and parser for each invocation (new message)
-    chain = CHAT_PROMPT_TEMPLATE | llm | parser
+async def call_model(state: MessagesState):
+    print("Calling model")
+    chain = PROMPT | llm
+    response = await chain.ainvoke(state)
+    return {"messages": response}
 
-    return chain
+
+# Define the (single) node in the graph
+workflow.add_edge(START, "model")
+workflow.add_node("model", call_model)
+
+# Add memory
+memory = MemorySaver()
+app = workflow.compile(checkpointer=memory)
