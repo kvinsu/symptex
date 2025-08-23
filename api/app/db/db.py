e08f514d0b184c318fb4766cd616488aea748b6a
kvinsu <kevinflorian.su@gmail.com>
2025-08-24 00:58:17 +0200

    Add db connection to ilvi

---
+++ api/app/db/db.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "postgresql://ilvi:ilvi@postgres:5432/ilvi"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+# Database dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        pass
